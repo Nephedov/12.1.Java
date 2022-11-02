8ca273745bf6663a198ac99d12f76bc6b6514ea8
Mihail Nephedov <Nephedov93@gmail.com>
2022-11-02 23:51:13 +0300

    edit constructor in PosterManager.class

--- src/main/java/ru/netology/PosterManager.java
+++ src/main/java/ru/netology/PosterManager.java
@@ -5,12 +5,19 @@ import ru.netology.repository.PosterRepository;
 
 public class PosterManager {
     private PosterRepository repo;
+    private int maxLimit;
+
 
     public PosterManager(PosterRepository repo) {
         this.repo = repo;
+        this.maxLimit = 10;
     }
 
-    private int maxLimit = 10;
+    public PosterManager(PosterRepository repo, int maxLimit) {
+        this.repo = repo;
+        this.maxLimit = maxLimit;
+    }
+
 
     public void add(Film film) {
         repo.save(film);
@@ -21,24 +28,6 @@ public class PosterManager {
     }
 
     public Film[] getLast() {
-        Film[] allFilms = getAll();
-
-        int resultLength;
-        if (maxLimit > allFilms.length) {
-            resultLength = allFilms.length;
-        } else {
-            resultLength = maxLimit;
-        }
-
-        Film[] reverseFilms = new Film[resultLength];
-        for (int i = 0; i < reverseFilms.length; i++) {
-            reverseFilms[i] = allFilms[allFilms.length - 1 - i];
-        }
-        return reverseFilms;
-    }
-
-    public Film[] getLast(int maxLimit) {
-
         Film[] allFilms = getAll();
 
         int resultLength;
--- src/test/java/ru/netology/PosterManagerTest.java
+++ src/test/java/ru/netology/PosterManagerTest.java
@@ -14,9 +14,15 @@ public class PosterManagerTest {
     PosterRepository repo = Mockito.mock(PosterRepository.class);
     PosterManager manager = new PosterManager(repo);
 
+    PosterRepository repo1 = new PosterRepository();
+    PosterManager manager2 = new PosterManager(repo1, 5);
+
     Film film1 = new Film(1, "Film1");
     Film film2 = new Film(2, "Film2");
     Film film3 = new Film(3, "Film3");
+    Film film4 = new Film(4, "Film4");
+    Film film5 = new Film(5, "Film5");
+    Film film6 = new Film(6, "Film6");
 
     @Test
     void getAllMockTest() {
@@ -49,7 +55,22 @@ public class PosterManagerTest {
         doReturn(films).when(repo).findAll();
 
         Film[] expected = {film3};
-        Film[] actual = manager.getLast(1);
+        Film[] actual = manager.getLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void getLastTest() {
+        manager2.add(film1);
+        manager2.add(film2);
+        manager2.add(film3);
+        manager2.add(film4);
+        manager2.add(film5);
+        manager2.add(film6);
+
+        Film[] expected = {film6, film5, film4, film3, film2};
+        Film[] actual = manager2.getLast();
 
         Assertions.assertArrayEquals(expected, actual);
     }
